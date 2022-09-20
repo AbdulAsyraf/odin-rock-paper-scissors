dfbb989082bde1c625cabd4e3c80b52fbf6314b1
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-21 02:19:27 +0800

    Finished the RPS ui
    Done with the UI for the rock paper scissors game
    Playing is now done by clicking the buttons
    Game is over once a player reaches 5 and the score resets

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
+let playerScore = 0;
+let botScore = 0;
+
 function playRound(playerHand, computerHand){
     console.log(`Computer player ${computerHand}`);
     if (playerHand == computerHand){
-        return ("It's a draw!");
+        console.log("It's a draw!");
     }else if ((playerHand == "rock" && computerHand == "paper") || (playerHand == "paper" && computerHand == "scissors") || (playerHand == "scissors" && computerHand == "rock")){
-        return (`You lose! ${computerHand} beats ${playerHand}`);
+        console.log(`You lose! ${computerHand} beats ${playerHand}`);
+        updateScore("bot");
     }else if ((computerHand == "rock" && playerHand == "paper") || (computerHand == "paper" && playerHand == "scissors") || (computerHand == "scissors" && playerHand == "rock")){
-        return (`You win! ${playerHand} beats ${computerHand}`);
+        console.log(`You win! ${playerHand} beats ${computerHand}`);
+        updateScore("player");
+    }
+}
+
+function updateScore(winner){
+    const playerScore = document.querySelector("#playerScore");
+    const botScore = document.querySelector("#botScore");
+
+    if (winner === "player"){
+        playerScore.textContent = parseInt(playerScore.innerHTML) + 1;
     }else {
-        return (`${playerHand} is an invalid input`);
+        botScore.textContent = parseInt(botScore.innerHTML) + 1;
+    }
+
+    let playerScoreNum = playerScore.textContent;
+    let botScoreNum = botScore.textContent;
+
+    if (botScoreNum == 5){
+        botScore.textContent = 0;
+        playerScore.textContent = 0;
+        alert("The computer won!");
+    }
+    if (playerScoreNum == 5){
+        botScore.textContent = 0;
+        playerScore.textContent = 0;
+        alert("You won!");
     }
 }
 
@@ -25,12 +53,7 @@ function generateComputerHand(){
     }
 }
 
-function game(){
-    for (let i = 0; i < 5; i++){
-        let playerHand = prompt(`Round ${i + 1}.\nYour hand?`).toLowerCase();
-        let result = playRound(playerHand, generateComputerHand());
-        console.log(result);
-    }
-}
-
-game();
+const options = document.querySelectorAll("button");
+options.forEach(button => button.addEventListener('click', function(e){
+    playRound(this.id, generateComputerHand());
+}));
